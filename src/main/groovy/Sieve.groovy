4e743efcda7e9e19a63713e832ef2dbf72635c41
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-18 16:21:37 +0100

    Sieve

--- src/main/groovy/Sieve.groovy
+++ src/main/groovy/Sieve.groovy
@@ -2,9 +2,9 @@
  * Created by mtumilowicz on 2018-11-18.
  */
 class Sieve {
-    static def get(n) {
-        def t = 2..n
-        (2..Math.sqrt(n)).each { k -> t -= ((2*k)..(t.last()+1)).step(k) }
-        return t
+    static def get(max) {
+        def sieve = 2..max
+        (2..Math.sqrt(max)).each { k -> sieve -= ((2*k)..(sieve.last()+1)).step(k) }
+        return sieve
     }
 }
