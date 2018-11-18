d66dbea959a232ae648ff98488427fc409cee1ad
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-18 16:55:25 +0100

    OperationsTest.groovy

--- src/test/groovy/OperationsTest.groovy
+++ src/test/groovy/OperationsTest.groovy
@@ -19,7 +19,7 @@ class OperationsTest extends Specification {
         expect:
         1..3 == [1, 2, 3]
     }
-    
+
     def "range operator 10..8"() {
         expect:
         10..8 == [10, 9, 8]
@@ -34,5 +34,37 @@ class OperationsTest extends Specification {
         expect:
         5..Math.sqrt(40) == [5, 6]
     }
-    
+
+    def "overloaded - on lists; contains"() {
+        expect:
+        [1] - 1 == []
+    }
+
+    def "overloaded - on lists; not contains"() {
+        expect:
+        [2] - 1 == [2]
+    }
+
+    def "step"() {
+        expect:
+        (1..10).step(5) == [1, 6]
+    }
+
+    def "step -1"() {
+        expect:
+        (1..3).step(-1) == [3, 2, 1]
+    }
+
+    def "step 0"() {
+        when:
+        (1..3).step(0)
+
+        then:
+        thrown(GroovyRuntimeException)
+    }
+
+    def "step 1"() {
+        expect:
+        (1..3).step(1) == [1, 2, 3]
+    }
 }
