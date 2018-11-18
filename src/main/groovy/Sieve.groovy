de280c37ba164abf2e7ce9ea6b16e850cfc4b93b
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-18 16:29:07 +0100

    Sieve renaming

--- src/main/groovy/Sieve.groovy
+++ src/main/groovy/Sieve.groovy
@@ -2,9 +2,9 @@
  * Created by mtumilowicz on 2018-11-18.
  */
 class Sieve {
-    static def get(max) {
-        def sieve = 2..max
-        (2..Math.sqrt(max)).each { k -> sieve -= ((2*k)..(sieve.last()+1)).step(k) }
+    static def get(limit) {
+        def sieve = 2..limit
+        (2..Math.sqrt(limit)).each { k -> sieve -= ((2*k)..(sieve.last()+1)).step(k) }
         return sieve
     }
 }
